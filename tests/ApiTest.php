bf7b102592e69e2e2bc6d9ac8b34166f74ed6280
Miroslav L <lemmas.online@gmail.com>
2018-10-08 12:13:38 +0300

    Added tests + travis ci

---
+++ tests/ApiTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace leRisen\tempmail\Tests; 
+
+use PHPUnit\Framework\TestCase;
+
+use leRisen\tempmail\TempMailApiClient;
+use leRisen\tempmail\TempMailApiResult;
+
+class ApiTest extends TestCase
+{
+    const API_KEY = 'qwerty';
+
+    private $api;
+
+    protected function setUp()
+    {
+        $this->api = new TempMailApiClient(self::API_KEY);
+    }
+
+    protected function tearDown()
+    {
+        $this->api = NULL;
+    }
+
+    public function testCallWithInvalidKey()
+    {
+        $request = $this->api->domainsList();
+        $result = $request->execute();
+
+        $this->assertInstanceOf(TempMailApiResult::class, $result);
+        $this->assertTrue($result->error);
+        $this->assertEquals($result->error_msg, 'Invalid Mashape Key');
+        $this->assertFalse($result->success);
+        $this->assertNull($result->response);
+    }
+}
